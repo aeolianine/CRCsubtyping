13a3c43802bc24f0d811b64d26c0ce9b64acd5e1
Gergana <gergana@Roger-Ganald.local>
2023-01-01 23:02:45 +0100

    refactoring and testing in get_data.r, load_signatures.r and normalize.r

--- R/normalize.R
+++ R/normalize.R
@@ -9,14 +9,13 @@
 #'         together with "refDataset" (pasted together column-wise)
 #' @export
 #' @examples
-#' print('combatToRef(): The examples are not written yet!')
 #' dat1 = cbind(c(1,2,3,3), c(-5,7,3,8), c(10,-4,5,0))
 #' dat2 = cbind(c(10,2.6,-30,39), c(-15,7.4,-3.2,10), c(5,4,-5,0.5))
 #' rownames(dat1) = c('g1', 'g2', 'g3', 'g4')
 #' rownames(dat2) = c('g1', 'g2', 'g3', 'g4')
-#' print(combatToRef(dat1, dat2))
+#' print(combat_to_ref(dat1, dat2))
 
-combatToRef = function(myDataset, refDataset){
+combat_to_ref = function(myDataset, refDataset){
 
   # make sure there are genes to intersect
   stopifnot(!is.null(rownames(myDataset)))
@@ -51,126 +50,3 @@ combatToRef = function(myDataset, refDataset){
 	return( matNorm )
 
 }
-
-#' M-combat with the GSE35896 dataset as reference.
-#' @param mat - the matrix to be normalized to GSE35896
-#' @return a list of two variables: the normalized dataset alone, and combined with GSE35896
-#' @export
-
-OLD_combatToGSE35896 = function(mat, plot = FALSE){
-
-	# remove zero rows from mat
-	zeros = rowSums(mat)==0
-	mat = mat[!zeros,]
-
-	y = loadGSE35896()$gex
-
-	if (isTRUE(all.equal(mat, y))){
-		print('combatToGSE35896(): no normalization performed, this is the reference dataset.')
-		return( list('alone'=mat, 'together'=mat) )
-	}
-
-	# remove zeros from GSE35896
-	zeros = rowSums(y)==0
-	y = y[!zeros,]
-
-	commonGenes = intersect(rownames(mat), rownames(y))
-	if (isTRUE(all.equal(mat[commonGenes,],y[commonGenes,]))){
-		return(list('alone' = mat))
-	}
-	matGSE = cbind(mat[commonGenes,], y[commonGenes,])
-
-	zeros = rowSums(matGSE)==0
-	matGSE = matGSE[!zeros,]
-
-	if (plot){
-		source('~//tools/generalPlottingTools.R')
-		par(mfrow=c(1,2))
-		MDS(matGSE, types = c(rep('data', ncol(mat)), rep('ref', ncol(y))), levelColors = c('red', 'blue'), title = 'before')
-		lim = par('usr')
-	}
-
-
-	library(sva, quietly=TRUE)
-	source('~//tools/M-ComBat/MComBatRScript.R') # get m-combat
-
-	temp = M.COMBAT(as.matrix(matGSE), batch = c(rep('data', ncol(mat)), rep('ref', ncol(y))), center = 'ref', mod = as.matrix( rep(1,ncol(matGSE)) ) )
-
-	if (plot){
-		MDS(temp[, c((ncol(mat)+1):ncol(temp), 1:ncol(mat))], types = c(rep('ref', ncol(y)), rep('data', ncol(mat))),
-			levelColors = c('red', 'blue'), xlim = c(lim[1], lim[2]), ylim = c(lim[3],lim[4]), title = 'after adjustment to GSE35896')
-		par(mfrow=c(1,1))
-	}
-
-
-	return( list('alone'=temp[,1:ncol(mat)], 'together'=temp) )
-}
-
-
-#' M-combat with the GSE13294 and the GSE14333 datasets as reference.
-#' @param mat - the matrix to be normalized to GSE13294/GSE14333
-#' @return a list of two variables: the normalized dataset alone, and combined with GSE13294/GSE14333
-
-OLD_combatToGSE13294_GSE14333 = function(mat, plot = FALSE){
-
-	# remove zero rows from mat
-	zeros = rowSums(mat)==0
-	mat = mat[!zeros,]
-
-	y1 = loadGSE13294()$gex
-	y2 = loadGSE14333()$gex
-
-	if (isTRUE(all.equal(y1, mat)) | isTRUE(all.equal(y2, mat))){
-		print('combatToGSE13294_GSE14333(): no normalization performed, this is the reference dataset.')
-		return( list('alone'=mat, 'together'=mat) )
-	}
-
-	# combat the two datasets together
-	library(sva, quietly=TRUE)
-
-    genesToRemove1 = which(rowSums(y1)==0)
-    genesToRemove2 = which(rowSums(y2)==0)
-
-    if (length(genesToRemove1)>0){ y1 = y1[-genesToRemove1, ] }
-    if (length(genesToRemove2)>0){ y2 = y2[-genesToRemove2, ] }
-
-    print(paste('combatToGSE13294_GSE14333(): removed',length(genesToRemove1),'zero-count genes from GSE13294.',sep=' '))
-	print(paste('combatToGSE13294_GSE14333(): removed',length(genesToRemove1),'zero-count genes from GSE14333.',sep=' '))
-    commonGenes = intersect(rownames(y1), rownames(y2))
-    y = as.matrix( cbind(y1[commonGenes,], y2[commonGenes,]) )
-
-    # combat
-    y = ComBat(y, batch = c(rep('1',ncol(y1)), rep('2',ncol(y2))), mod = c(rep(1,ncol(y))) )
-
-	# remove zeros from GSE13294 & GSE14333, there shouldn't be any
-	zeros = rowSums(y)==0
-	y = y[!zeros,]
-
-	commonGenes = intersect(rownames(mat), rownames(y))
-	matGSE = cbind(mat[commonGenes,], y[commonGenes,])
-
-	zeros = rowSums(matGSE)==0
-	matGSE = matGSE[!zeros,]
-
-	if (plot){
-		source('~//tools/generalPlottingTools.R')
-		par(mfrow=c(1,2))
-		MDS(matGSE, types = c(rep('data', ncol(mat)), rep('ref', ncol(y))), levelColors = c('red', 'blue'), title = 'before')
-		lim = par('usr')
-	}
-
-
-	library(sva, quietly=TRUE)
-	source('~//tools/M-ComBat/MComBatRScript.R') # get m-combat
-
-	temp = M.COMBAT(as.matrix(matGSE), batch = c(rep('data', ncol(mat)), rep('ref', ncol(y))), center = 'ref', mod = as.matrix( rep(1,ncol(matGSE)) ) )
-
-	if (plot){
-		MDS(temp[, c((ncol(mat)+1):ncol(temp), 1:ncol(mat))], types = c(rep('ref', ncol(y)), rep('data', ncol(mat))),
-			levelColors = c('red', 'blue'), xlim = c(lim[1], lim[2]), ylim = c(lim[3],lim[4]), title = 'after adjustment to GSE13294_GSE14333')
-		par(mfrow=c(1,1))
-	}
-
-	return( list('alone'=temp[,1:ncol(mat)], 'together'=temp) )
-}
-
--- tests/testthat/test_normalize.R
+++ tests/testthat/test_normalize.R
@@ -1,20 +1,20 @@
 context('Normalization tools')
 
-test_that('combatToRef works as intended.', {
+test_that('combat_to_ref() works as intended.', {
 
-  expect_error(combatToRef())
-  expect_error(combatToRef(dhdhnd))
-  expect_error(combatToRef('fgdhdhnd'))
-  expect_error(combatToRef(c(112,2,5,7,3)))
+  expect_error(combat_to_ref())
+  expect_error(combat_to_ref(dhdhnd))
+  expect_error(combat_to_ref('fgdhdhnd'))
+  expect_error(combat_to_ref(c(112,2,5,7,3)))
 
   mat = get_TCGA_synapse()
   out = cmdscale(as.dist(1-cor(mat, method = 'spearman')))
   group1 = rownames(out)[out[,1] > 0.015]
   group2 = rownames(out)[out[,1] <= -0.015]
 
-  matNorm = combatToRef(mat[,group1], mat[,group2])
+  matNorm = combat_to_ref(mat[,group1], mat[,group2])
   expect_equal(dim(matNorm), dim(mat[,c(group1,group2)]))
-  expect_equal(class(matNorm), 'matrix')
+  expect_equal(class(matNorm)[1], 'matrix')
 
   # clean up
   rm(mat, out, group1, group2, matNorm)
